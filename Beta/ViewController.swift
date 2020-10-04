0ad0de7e7c90f9292b5ce51fdd8d211bd46a5bf4
z3t4z00k <zetazook@gmail.com>
2020-10-04 18:24:47 +0530

    Xcode init

---
+++ Beta/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Beta
+//
+//  Created by Archit Agarwal on 04/10/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
